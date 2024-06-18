bfd0671da30a56a71f0b487fda47ebf2e4881036
omar mohamed <104155350+omarmo7077@users.noreply.github.com>
2024-06-18 15:35:39 +0300

    Add files via upload

---
+++ all.js
@@ -0,0 +1,15 @@
+// Dark & Light toggle
+
+document.querySelector(".day-night input").addEventListener("change", () => {
+  document.querySelector("body").classList.add("toggle");
+  setTimeout(() => {
+    document.querySelector("body").classList.toggle("light");
+
+    setTimeout(
+      () => document.querySelector("body").classList.remove("toggle"),
+      10
+    );
+  }, 5);
+});
+
+//
